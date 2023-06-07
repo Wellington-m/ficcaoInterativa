02e4c624787d223352fc21de07eea28b2119a62e
Wellington <wellingtonmachioni@gmail.com>
2023-06-07 16:10:33 -0300

    start

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function iniciarLeitura() {
+  // Redirecionar o usuário para a página 1 do livro
+  window.location.href = 'paginas/pagina1.html';
+}
+
+function decisao1() {
+  // Aqui você pode adicionar o código para a ação da Decisão 1
+  alert('Decisão 1 selecionada!');
+}
+
+function decisao2() {
+  // Aqui você pode adicionar o código para a ação da Decisão 2
+  alert('Decisão 2 selecionada!');
+}
